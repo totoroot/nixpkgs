b3042626e7298cd48f46bdfd978ad428f255cb87
Matthias Thym <git@thym.at>
2024-03-28 09:42:21 +0100

    flynt: 0.66 -> 1.0.1

--- pkgs/development/python-modules/flynt/default.nix
+++ pkgs/development/python-modules/flynt/default.nix
@@ -2,33 +2,41 @@
 , buildPythonPackage
 , pythonOlder
 , fetchFromGitHub
+, hatchling
 , astor
 , pytestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "flynt";
-  version = "0.66";
-  format = "setuptools";
+  version = "1.0.1";
+  format = "pyproject";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "ikamensh";
     repo = "flynt";
     rev = version;
-    hash = "sha256-DV433wqLjF5k4g8J7rj5gZfaw+Y4/TDOoFKo3eKDjZ4=";
+    hash = "sha256-UHY4UDBHcP3ARikktIehSUD3Dx8A0xpOnfKWWrLCsOY=";
   };
+
+  nativeBuildInputs = [
+    hatchling
+  ];
 
   propagatedBuildInputs = [ astor ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
   meta = with lib; {
-    description = "command line tool to automatically convert a project's Python code from old format style strings into Python 3.6+'s f-strings";
+    description = "Tool to automatically convert old string literal formatting to f-strings in Python code";
+    longDescription = ''
+      flynt is a command line tool to automatically convert a project's Python code from old "%-formatted" and .format(...) strings into Python 3.6+'s "f-strings".
+    '';
     mainProgram = "flynt";
     homepage = "https://github.com/ikamensh/flynt";
     license = licenses.mit;
-    maintainers = with maintainers; [ cpcloud ];
+    maintainers = with maintainers; [ cpcloud totoroot ];
   };
 }
